c9a7cc1e8f4716e2623e8e9b67f8c3ab99412008
Fwad Abdi <112805183+Fwad974@users.noreply.github.com>
2023-12-08 21:26:27 +0330

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ BOT_TOKEN = "6759949264:AAGZU-NAkP72r8U6VfI8IoMDlrk3I6O8Uo8"
 bot = telebot.TeleBot(BOT_TOKEN)
 
 USER_STATE = {}
-number_of_utterances=100
+number_of_utterances=10
 def send_gender_keyboard(chat_id):
     markup = types.InlineKeyboardMarkup()
     male_button = types.InlineKeyboardButton("Male", callback_data="gender_male")
@@ -58,6 +58,32 @@ def handle_query(call):
         USER_STATE[user_id]["stage"] = "recording"
         bot.send_message(call.message.chat.id, f"Please record and send {number_of_utterances} utterances.")
 
+    user_data = USER_STATE.get(user_id, {})
+
+    if call.data == "submit_voice" and "current_voice" in user_data:
+        # Save the voice message
+        file_info = bot.get_file(user_data["current_voice"])
+        downloaded_file = bot.download_file(file_info.file_path)
+        with open(f"user_{user_id}_utterance_{user_data['utterances_recorded']}.ogg", 'wb') as new_file:
+            new_file.write(downloaded_file)
+
+        USER_STATE[user_id]["utterances_recorded"] += 1
+        del USER_STATE[user_id]["current_voice"]  # Clear the current voice reference
+
+        # Check if all utterances are recorded
+        if user_data["utterances_recorded"] >= number_of_utterances:
+            bot.send_message(call.message.chat.id, "Thank you for recording all the utterances.")
+            USER_STATE[user_id]["stage"] = "completed"
+        else:
+            remaining = number_of_utterances - user_data["utterances_recorded"]
+            bot.send_message(call.message.chat.id, f"Please record and send {remaining} more utterance(s).")
+
+    elif call.data == "re_record_voice":
+        # Clear the current voice reference and prompt for re-recording
+        if "current_voice" in user_data:
+            del USER_STATE[user_id]["current_voice"]
+        bot.send_message(call.message.chat.id, "Please re-record your utterance.")
+
     # elif call.data.startswith("education_"):
     #     USER_STATE[user_id]["education"] = call.data.split("_")[1]
     #     USER_STATE[user_id]["stage"] = "completed"
@@ -84,7 +110,7 @@ def handle_voice(message):
         else:
             remaining = number_of_utterances - USER_STATE[user_id]["utterances_recorded"]
             bot.send_message(message.chat.id, f"Please record and send {remaining} more utterance(s).")
-
+            
 
 
 @bot.message_handler(func=lambda message: True)
