8d923ed6656603704dce74874ddb545a12fbc610
Fwad Abdi <112805183+Fwad974@users.noreply.github.com>
2023-12-23 10:57:43 +0330

    Update main2.py

--- main2.py
+++ main2.py
@@ -155,7 +155,7 @@ def handle_query(call):
     if call.data == "continue_recording":
         USER_STATE[user_id]["prompt_time"] = time.time()
         remaining = number_of_utterances - user_data["utterances_recorded"]
-        bot.edit_message_text(chat_id=call.message.chat.id, message_id=USER_STATE[user_id]['last_message_id'], text="لطفا جمله "+UTT_LIST[remaining]+" را دوباره ضبط کنید."
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=USER_STATE[user_id]['last_message_id'], text="لطفا جمله "+UTT_LIST[remaining]+" را دوباره ضبط کنید.")
         remaining = number_of_utterances - user_data["utterances_recorded"]
         timer = threading.Thread(target=send_expiry_message, args=(call.message.chat.id,remaining))
         timer.start()
